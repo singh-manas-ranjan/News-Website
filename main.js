ec6ebba30da966affad6d85aa36188c8e0427148
singh-manas-ranjan <manassingh900@gmail.com>
2023-06-29 14:30:52 +0530

    remove console.log code

--- main.js
+++ main.js
@@ -7,7 +7,6 @@ const month = String(date.getMonth()+1).padStart(2,'0');
 const day = String(date.getDate()-1).padStart(2,'0');
 
 const queryDate = `${fullYear}-${month}-${day}`;
-console.log(queryDate); 
 
 // ======================== ON LOAD WINDOW FETCHING DATA FROM NEWS API ========================
 
